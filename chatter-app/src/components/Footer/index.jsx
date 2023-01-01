bb3081a8b80974ce8423ec499f79a41859ad21d4
whosmudassir <deadsoulartist@gmail.com>
2023-01-01 12:33:56 +0530

    feat: header, footer for login ui

---
+++ chatter-app/src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import "./index.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-icon">
+      Built with by <FavoriteIcon /> Mudassir Khan
+    </div>
+  );
+};
+
+export default Footer;
---
+++ chatter-app/src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import "./index.css";
+
+const Header = () => {
+  return (
+    <div className="header-icon">
+      <GitHubIcon />
+    </div>
+  );
+};
+
+export default Header;
