dfcf0f7e209e5b47f1331ecf9714af3bdc462a73
whosmudassir <deadsoulartist@gmail.com>
2023-01-02 18:24:06 +0530

    fix: updated icon colors

--- chatter-app/src/components/Footer/index.jsx
+++ chatter-app/src/components/Footer/index.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import FavoriteIcon from "@material-ui/icons/Favorite";
+import { withStyles } from "@material-ui/core/styles";
 import "./index.css";
 
 const Footer = () => {
+  const StyledFavoriteIcon = withStyles({
+    root: {
+      color: "#ed2939",
+      fontSize: "18px",
+      padding: "0px 2px",
+    },
+  })(FavoriteIcon);
+
   return (
     <div className="footer-icon">
-      Built with by <FavoriteIcon /> Mudassir Khan
+      Built with <StyledFavoriteIcon /> by Mudassir Khan
     </div>
   );
 };
